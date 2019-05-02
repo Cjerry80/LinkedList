d5a9f1930ef58ae97491ed63b8b2392d88ef1a7c
cjerry80 <cjerry80@cs3006b.acad.fullerton.edu>
2019-05-02 16:50:46 -0700

    Modified readfile

--- main.cpp
+++ main.cpp
@@ -5,10 +5,21 @@
 using namespace std;
 
 void readFile(linkedlist & list, string & file) {
+ifstream infile;
+int n1;
+infile.open("list.txt");
+while(!infile.eof()){
+infile >> n1;
+addToFront(n1);
+}
+infile.close();
+
+
 
 }
 
 int main() {
+readfile(list, file);
 
     return 0;
 }
